34e6b04feb0c08340c6ae7ae72a3d3cf95a5b7ef
trong-le <trong.p.le.92@gmail.com>
2015-12-26 23:08:16 -0500

    Fix popover history clearing each time from new MVC every time button is pressed

--- Psychologist/DiagnosedHappinessViewController.swift
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -10,4 +10,42 @@ import UIKit
 
 class DiagnosedHappinessViewController: HappinessViewController {
     
+    // Override superclass HappinessViewController and add into diagnosticHistory array
+    override var happiness: Int {
+        didSet {
+            diagnosticHistory += [happiness]
+        }
+    }
+    
+    // History of buttons, have to do this way or else MVC is recreated every time clearing history
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var diagnosticHistory: [Int] {
+        get {
+            // Make sure array of ints else empty array
+            return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? []
+        }
+        set {
+            defaults.setObject(newValue, forKey: History.DefaultsKey)
+        }
+    }
+    
+    private struct History {
+        static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
+    }
+    
+    // Display diagnostic history
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    tvc.text = "\(diagnosticHistory)"
+                }
+            default: break
+            }
+        }
+    }
+    
 }
